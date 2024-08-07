64e017f66883640712d336ca0de7d59f095fe12b
GabrielPastore <106038103+GabrielPastore@users.noreply.github.com>
2024-08-07 15:15:11 -0300

    Update defs.py

--- defs.py
+++ defs.py
@@ -2,12 +2,12 @@ from requests import get
 
 API_KEY = "62XA11P6M2XFIXWD9SKARQTMFQMKY8FP6K"
 
-def make_api_url(**kwargs):
+def make_api_url(module, action, **kwargs):
     BASE_URL = "https://api.etherscan.io/api"
-    BASE_URL += "?module=account&action=balance"
-    for item in kwargs:
-        BASE_URL += f"&{item}={kwargs[item]}"
+    BASE_URL += f"?module={module}&action={action}"
+    for k, v in kwargs.items():
+        BASE_URL += f"&{k}={v}"
     BASE_URL += f"&apikey={API_KEY}"
     return BASE_URL
 
-make_api_url(address="0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae", tag="latest")
+print(make_api_url("account", "balance", address="0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae", tag="latest"))
